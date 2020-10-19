5945ac9c89a51e132096fb006fdc887c1a6dc042
TEAM-PIKACHU-INDIA <73126271+TEAM-PIKACHU-INDIA@users.noreply.github.com>
2020-10-19 22:02:56 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,27 +1,29 @@
-
-"""
-.alive 
-For all USER by Team Pikachu
-"""
 import asyncio
 from telethon import events
+from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
-from uniborg.util import admin_cmd
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://telegra.ph/file/00f47c54ceda3944dc47c.jpg"
+pm_caption = "**🔥ᴘɪᴋᴀᴄʜᴜ ɪs ᴏɴʟɪɴᴇ❤**\n"
 
+pm_caption += f"**M̴y̴ ̴B̴o̴s̴s̴**            : {DEFAULTUSER}\n"
 
-@borg.on(admin_cmd(pattern="alive"))
-#@borg.on(events.NewMessage(pattern=r"\.alive(.*)",incoming=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "`Yoss, I'm alive 🔥❤`"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
+pm_caption += "ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ        :  15.0.0 \n"
+
+pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ          : [ᴊᴏɪɴ](https://t.me/)\n"
+
+pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ        : [ᴊᴏɪɴ](https://t.me/)\n"
+
+pm_caption += "ʟɪᴄᴇɴꜱᴇ                 : [ʟɪᴄᴇɴꜱᴇ](https://github.com/TEAM-PIKACHU-INDIA/PIKACHU-THE-USERBOT/blob/master/LICENSE)\n"
+
+pm_caption += "ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ             : [ᴛᴇᴀᴍ ᴘɪᴋᴀᴄʜᴜ](https://github.com/team-pikachu-india)\n"
+
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    chat = await alive.get_chat()
+    await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
